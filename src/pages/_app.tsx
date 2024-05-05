fa9cd972292d3e212d3b754a1090983189db674e
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2024-05-05 14:27:55 +0200

    feat: intitialize new project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
+};
+
+export default api.withTRPC(MyApp);
